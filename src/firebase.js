6e33a4fd5c878d2c1df7ea9559357234e39dad6c
unknown <ekberprobsekber1@gmail.com>
2026-05-20 21:34:44 +0500

    nigaga

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth"; // Auth xizmatini import qilish
-import { getDatabase } from "firebase/database"; // Realtime Database xizmatini import qilish
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCeUJhralcgl6VxLz_cQUBk7r8c2WPF3-k",
+  apiKey: "AIzaSyCeUJhralcgl6VxLz_cQUBk7r8c2WPF3-k", // O'zingizniki bilan almashtiring
   authDomain: "moggame-backend.firebaseapp.com",
   databaseURL: "https://moggame-backend-default-rtdb.firebaseio.com",
   projectId: "moggame-backend",
   storageBucket: "moggame-backend.firebasestorage.app",
   messagingSenderId: "49451269798",
   appId: "1:49451269798:web:8111988c5baef14098fe67",
-  measurementId: "G-JXEBD2G0N6"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// MUHIM: Mana bu qatorlarni qo'shing (export qilish)
 export const auth = getAuth(app);
 export const db = getDatabase(app);
